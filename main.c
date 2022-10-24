6defa17983133591ac6f8072928bfcd9146c2a66
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-24 09:57:05 +0300

    Add Funcitons section

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+// compile: cc main.c -o main.exe
+// run: .\main.exe
+
+// Function signature required if the function is defined after main()
+int addNumbers(int num1, int num2);
 
 int main() {
 
-    // PRINTING
-    printf("Hello\n");
-    printf("world");
-    printf("!\n");
+    // FUNCTIONS
+    int sum = addNumbers(4, 60);
+    printf("The sum is: %d\n", sum);
+
+    // // PRINTING
+    // printf("Hello\n");
+    // printf("world");
+    // printf("!\n");
     return 0;
+}
+
+int addNumbers(int num1, int num2) {
+    return num1 + num2;
 }
